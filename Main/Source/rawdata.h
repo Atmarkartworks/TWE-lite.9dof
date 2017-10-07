dd458e2ba32060cdf825015042e5b8787bc8885a
aaw <aaw@aaw-PC>
2017-10-07 15:31:38 +0900

    9dof

---
+++ Main/Source/rawdata.h
@@ -0,0 +1,30 @@
+/*
+ * rawdata.h
+ *
+ *  Created on: 2017/10/07
+ *      Author: aaw
+ */
+
+#ifndef RAWDATA_H_
+#define RAWDATA_H_
+
+
+#if defined __cplusplus
+extern "C" {
+#endif
+
+
+void setup(void);
+void action(void);
+
+
+
+
+
+#if defined __cplusplus
+}
+#endif
+
+
+
+#endif /* RAWDATA_H_ */
