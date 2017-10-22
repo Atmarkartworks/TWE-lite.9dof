37df38a29eaa1c4cdbd8884a6482a091fea17121
aaw <aaw@aaw-PC>
2017-10-22 10:28:25 +0900

    driver

--- Main/Source/Euler.cpp
+++ Main/Source/Euler.cpp
@@ -44,8 +44,10 @@
 * patent rights of the copyright holder.
 */
 
+
 #include "NineAxesMotion.h"        //Contains the bridge code between the API and the Arduino Environment
 //#include <Wire.h>
+
 
 NineAxesMotion mySensor;         //Object that for the sensor
 unsigned long lastStreamTime = 0;     //To store the last streamed time stamp
